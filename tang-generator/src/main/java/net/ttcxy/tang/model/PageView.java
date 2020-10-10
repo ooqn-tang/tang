f60f9a082830c680720bc96c51e55f192ffcc7d8
one-few <792190997@qq.com>
2020-10-10 22:28:31 +0800

    提交一小波

---
+++ tang-generator/src/main/java/net/ttcxy/tang/model/PageView.java
@@ -0,0 +1,64 @@
+package net.ttcxy.tang.model;
+
+import io.swagger.annotations.ApiModelProperty;
+import java.io.Serializable;
+import java.util.Date;
+
+public class PageView implements Serializable {
+    private String userId;
+
+    private String blogId;
+
+    private Date createDatetime;
+
+    private String id;
+
+    private static final long serialVersionUID = 1L;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getBlogId() {
+        return blogId;
+    }
+
+    public void setBlogId(String blogId) {
+        this.blogId = blogId;
+    }
+
+    public Date getCreateDatetime() {
+        return createDatetime;
+    }
+
+    public void setCreateDatetime(Date createDatetime) {
+        this.createDatetime = createDatetime;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", userId=").append(userId);
+        sb.append(", blogId=").append(blogId);
+        sb.append(", createDatetime=").append(createDatetime);
+        sb.append(", id=").append(id);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
