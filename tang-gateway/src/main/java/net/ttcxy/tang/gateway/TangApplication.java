b48cbbd08da2708ebddf2a1f70c97531a732ba24
one-few <792190997@qq.com>
2020-11-01 19:28:52 +0800

    提交一次

--- tang-gateway/src/main/java/net/ttcxy/tang/gateway/TangApplication.java
+++ tang-gateway/src/main/java/net/ttcxy/tang/gateway/TangApplication.java
@@ -1,9 +1,13 @@
 package net.ttcxy.tang.gateway;
 
+import cn.hutool.core.util.ArrayUtil;
 import net.ttcxy.tang.gateway.config.MyApplicationContextInitializer;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author huanglei
@@ -13,10 +17,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TangApplication {
 
     public static void main(String[] args) {
-        SpringApplication springApplication = new SpringApplication(TangApplication.class);
-        //关键一步：项目启动前调用我
-        springApplication.addInitializers(new MyApplicationContextInitializer());
-        springApplication.run(args);
+//        SpringApplication springApplication = new SpringApplication(TangApplication.class);
+//        //关键一步：项目启动前调用我
+//        springApplication.addInitializers(new MyApplicationContextInitializer());
+//        springApplication.run(args);
+
+        List<String> strings = Arrays.asList("1", "2", "2");
+
     }
 
 
