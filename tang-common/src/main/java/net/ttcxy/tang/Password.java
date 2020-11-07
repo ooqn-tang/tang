c1a9d26eb762bb6c01fd1ccd11efbc396e02777e
17674785177 <mima135654>
2020-11-07 18:31:01 +0800

    修改

--- tang-common/src/main/java/net/ttcxy/tang/Password.java
+++ tang-common/src/main/java/net/ttcxy/tang/Password.java
@@ -1,13 +1,11 @@
 package net.ttcxy.tang;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
 /**
  * 制作数据库密码
  * @author huanglei
  */
 public class Password {
     public static void main(String[] args) {
-        System.out.println(new BCryptPasswordEncoder().encode("123456789"));;
+
     }
 }
