2f40e3b602bdadbf6722886c8ab0016baac2c73f
one-few <792190997@qq.com>
2020-11-30 18:03:22 +0800

    提交大量代码

---
+++ tang-admin/src/test/java/TestMain.java
@@ -0,0 +1,23 @@
+import net.ttcxy.tang.admin.AdminApplication;
+import net.ttcxy.tang.service.StsMailService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author huanglei
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = AdminApplication.class)
+public class TestMain {
+
+    @Autowired
+    StsMailService stsMailService;
+
+    @Test
+    public void test1(){
+        System.out.println(stsMailService.sendMail("792190997@qq.com","test","test"));
+    }
+}
--- tang-common/src/main/java/net/ttcxy/tang/service/impl/StsMailServiceQQImpl.java
+++ tang-common/src/main/java/net/ttcxy/tang/service/impl/StsMailServiceQQImpl.java
@@ -3,6 +3,7 @@ package net.ttcxy.tang.service.impl;
 import cn.hutool.extra.mail.MailAccount;
 import cn.hutool.extra.mail.MailUtil;
 import net.ttcxy.tang.service.StsMailService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 /**
@@ -10,16 +11,48 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class StsMailServiceQQImpl implements StsMailService {
+
+    @Value("${mail.from}")
+    private String from;
+    @Value("${mail.user}")
+    private String user;
+    @Value("${mail.password}")
+    private String password;
     @Override
     public boolean sendMail(String mail, String title, String body) {
         MailAccount account = new MailAccount();
         account.setHost("smtp.qq.com");
         account.setPort(25);
         account.setAuth(true);
-        account.setFrom("1604403854@qq.com");
-        account.setUser("1604403854@qq.com");
-        account.setPass("muijiqqfyyyyhbhc");
+        account.setFrom(from);
+        account.setUser(user);
+        account.setPass(password);
+
         MailUtil.send(account, mail, title, body, false);
         return false;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
