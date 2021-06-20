9fa6a1bc5449e1a22bacb492436e3c153069b777
17674785177 <mima135654>
2021-06-20 23:36:52 +0800

    前端博客增删改查

---
+++ tang-web-gateway/src/views/404.vue
@@ -0,0 +1,28 @@
+<template>
+ <div>404</div>
+</template>
+
+<script>
+export default {
+  name: "error",
+  data() {
+    return {
+     
+    };
+  },
+  components: {
+
+  },
+  methods: {
+  },
+  mounted(){
+   
+  }
+};
+</script>
+
+<style lang="scss">
+body{
+    overflow-y: scroll;
+}
+</style>
