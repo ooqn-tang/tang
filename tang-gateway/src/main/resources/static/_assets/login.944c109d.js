5460bd0ab4f260790ab0eeee7d725a1b8d0c56d7
17674785177 <mima135654>
2021-07-12 19:30:30 +0800

    提交

---
+++ tang-gateway/src/main/resources/static/_assets/login.944c109d.js
@@ -0,0 +1,1 @@
+import{s as o}from"./index.6bb83b22.js";function t(t){return o({url:"/api/login",method:"post",params:t})}function r(){return o({url:"/api/logout",method:"get"})}export{t as a,r as l};
