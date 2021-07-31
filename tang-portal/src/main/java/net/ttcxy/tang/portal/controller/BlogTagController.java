f5eb866dfead9c2643b31ddc6f4c1cb3a67619f3
17674785177 <mima135654>
2021-07-31 23:51:20 +0800

    删除代码

--- tang-portal/src/main/java/net/ttcxy/tang/portal/controller/BlogTagController.java
+++
@@ -1,9 +0,0 @@
-package net.ttcxy.tang.portal.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("tag")
-public class BlogTagController {
-}
