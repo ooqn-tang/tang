4e37e349546acace59ad4794ea1c885fd8fcefd5
one-few <792190997@qq.com>
2021-10-08 21:31:35 +0800

    提交大量代码

--- tang-portal/src/main/resources/static/_assets/author.126194e0.js
+++
@@ -1,1 +0,0 @@
-import{s as r}from"./index.7f4a8142.js";function t(t){return r({url:"/api/author/"+t,method:"GET"})}function a(t){return r({url:"/api/author",method:"PUT",data:t})}export{t as s,a as u};
