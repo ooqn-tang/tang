b64eff88b1313ffb31ca5bb1044d73c644752e35
one-few <792190997@qq.com>
2021-11-20 17:47:06 +0800

    修复Https的302导致搜索引擎不搜录的问题。

---
+++ tang-portal/src/main/resources/static/_assets/author.c2ce0647.js
@@ -0,0 +1,1 @@
+import{s as r}from"./index.16955009.js";function t(t){return r({url:"/api/author/"+t,method:"GET"})}function a(t){return r({url:"/api/author",method:"PUT",data:t})}export{t as s,a as u};
