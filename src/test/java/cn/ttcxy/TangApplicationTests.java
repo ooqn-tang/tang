69ead7a63ff5341c4f4f72c3d174bb6f111c0cf2
OneFew <38550226+one-few@users.noreply.github.com>
2022-08-14 03:36:38 +0800

    提交大量代码

--- src/test/java/cn/ttcxy/TangApplicationTests.java
+++ src/test/java/cn/ttcxy/TangApplicationTests.java
@@ -1,9 +1,13 @@
 package cn.ttcxy;
 
+import cn.hutool.db.nosql.redis.RedisDS;
+import cn.hutool.setting.Setting;
 import cn.ttcxy.mapper.dao.DtsArticleDao;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
 
 @SpringBootTest
 class TangApplicationTests {
@@ -16,4 +20,11 @@ class TangApplicationTests {
 		System.out.println(dtsArticleDao.selectArticleById(""));
 	}
 
+	public static void main(String[] args) {
+		RedisDS redisDS = RedisDS.create();
+
+		redisDS.setStr("test","asdfasdf");
+
+		System.out.println(redisDS.getStr("test"));
+	}
 }
