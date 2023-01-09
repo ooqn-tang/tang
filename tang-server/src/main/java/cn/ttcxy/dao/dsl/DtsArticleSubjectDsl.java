894f112351ac3682ea97f17f63c858b1fe5bd093
bobcbui <bobcbui@outlook.com>
2023-01-09 20:49:58 +0800

    123456

--- tang-server/src/main/java/cn/ttcxy/dao/dsl/DtsArticleSubjectDsl.java
+++ tang-server/src/main/java/cn/ttcxy/dao/dsl/DtsArticleSubjectDsl.java
@@ -23,133 +23,133 @@ import org.springframework.stereotype.Component;
 @Component
 public class DtsArticleSubjectDsl {
 
-    private final QDtsArticleSubject qArticleSubject = QDtsArticleSubject.dtsArticleSubject;
-    private final QUtsAuthor qAuthor = QUtsAuthor.utsAuthor;
-    private final QDtsArticleSubjectRelevance qSubjectRelevance = QDtsArticleSubjectRelevance.dtsArticleSubjectRelevance;
-    private final QDtsArticle qArticle = QDtsArticle.dtsArticle;
+	private final QDtsArticleSubject qArticleSubject = QDtsArticleSubject.dtsArticleSubject;
+	private final QUtsAuthor qAuthor = QUtsAuthor.utsAuthor;
+	private final QDtsArticleSubjectRelevance qSubjectRelevance = QDtsArticleSubjectRelevance.dtsArticleSubjectRelevance;
+	private final QDtsArticle qArticle = QDtsArticle.dtsArticle;
 
-    @Autowired
-    private JPAQueryFactory query;
+	@Autowired
+	private JPAQueryFactory query;
 
-    public DtsArticleSubjectDto findSubjectById(String subjectId) {
-        return query
-                .select(Projections.bean(
-                        DtsArticleSubjectDto.class,
-                        qArticleSubject.subjectId,
-                        qArticleSubject.subjectName,
-                        qArticleSubject.synopsis,
-                        qArticleSubject.createDate,
-                        qAuthor.nickname,
-                        qAuthor.username))
-                .from(qArticleSubject, qAuthor)
-                .where(
-                        qAuthor.authorId.eq(qArticleSubject.authorId),
-                        qArticleSubject.subjectId.eq(subjectId))
-                .fetchOne();
-    }
+	public DtsArticleSubjectDto findSubjectById(String subjectId) {
+		return query
+				.select(Projections.bean(
+						DtsArticleSubjectDto.class,
+						qArticleSubject.subjectId,
+						qArticleSubject.subjectName,
+						qArticleSubject.synopsis,
+						qArticleSubject.createDate,
+						qAuthor.nickname,
+						qAuthor.username))
+				.from(qArticleSubject, qAuthor)
+				.where(
+						qAuthor.authorId.eq(qArticleSubject.authorId),
+						qArticleSubject.subjectId.eq(subjectId))
+				.fetchOne();
+	}
 
-    public Page<DtsArticleSubjectDto> findSubjectList(Pageable pageable) {
-        JPAQuery<?> jpaQuery = query
-                .from(qArticleSubject, qAuthor)
-                .where(qAuthor.authorId.eq(qArticleSubject.authorId));
+	public Page<DtsArticleSubjectDto> findSubjectList(Pageable pageable) {
+		JPAQuery<?> jpaQuery = query
+				.from(qArticleSubject, qAuthor)
+				.where(qAuthor.authorId.eq(qArticleSubject.authorId));
 
-        Long fetchOne = jpaQuery
-                .select(qArticleSubject.subjectId.count())
-                .fetchOne();
+		Long fetchOne = jpaQuery
+				.select(qArticleSubject.subjectId.count())
+				.fetchOne();
 
-        List<DtsArticleSubjectDto> articleSubjectList = jpaQuery
-                .select(Projections.bean(
-                        DtsArticleSubjectDto.class,
-                        qArticleSubject.subjectId,
-                        qArticleSubject.subjectName,
-                        qArticleSubject.synopsis,
-                        qArticleSubject.createDate,
-                        qArticleSubject.updateDate))
-                .offset(pageable.getOffset())
-                .limit(pageable.getPageSize())
-                .fetch();
+		List<DtsArticleSubjectDto> articleSubjectList = jpaQuery
+				.select(Projections.bean(
+						DtsArticleSubjectDto.class,
+						qArticleSubject.subjectId,
+						qArticleSubject.subjectName,
+						qArticleSubject.synopsis,
+						qArticleSubject.createDate,
+						qArticleSubject.updateDate))
+				.offset(pageable.getOffset())
+				.limit(pageable.getPageSize())
+				.fetch();
 
-        return new PageImpl<>(articleSubjectList, pageable, fetchOne);
-    }
+		return new PageImpl<>(articleSubjectList, pageable, fetchOne);
+	}
 
-    public List<DtsArticleSubjectDto> findSubjectListByUsername(String username) {
-        JPAQuery<?> jpaQuery = query
-                .from(qArticleSubject)
-                .leftJoin(qAuthor).on(
-                        qAuthor.authorId.eq(qArticleSubject.authorId))
-                .where(qAuthor.username.eq(username));
+	public List<DtsArticleSubjectDto> findSubjectListByUsername(String username) {
+		JPAQuery<?> jpaQuery = query
+				.from(qArticleSubject)
+				.leftJoin(qAuthor).on(
+						qAuthor.authorId.eq(qArticleSubject.authorId))
+				.where(qAuthor.username.eq(username));
 
-        return jpaQuery.select(Projections.bean(
-                DtsArticleSubjectDto.class,
-                qArticleSubject.subjectId,
-                qArticleSubject.subjectName,
-                qArticleSubject.synopsis,
-                qArticleSubject.createDate,
-                qArticleSubject.updateDate,
-                qArticleSubject.authorId,
-                qAuthor.username,
-                qAuthor.nickname))
-                .fetch();
-    }
+		return jpaQuery.select(Projections.bean(
+				DtsArticleSubjectDto.class,
+				qArticleSubject.subjectId,
+				qArticleSubject.subjectName,
+				qArticleSubject.synopsis,
+				qArticleSubject.createDate,
+				qArticleSubject.updateDate,
+				qArticleSubject.authorId,
+				qAuthor.username,
+				qAuthor.nickname))
+				.fetch();
+	}
 
-    public Page<DtsArticleSubjectDto> findSubjectListBySubjectName(String name, Pageable pageable) {
-        JPAQuery<?> jpaQuery = query
-                .from(qArticleSubject)
-                .leftJoin(qAuthor).on(
-                        qAuthor.authorId.eq(qArticleSubject.authorId))
-                .where(qArticleSubject.subjectName.like('%' + name + '%'));
+	public Page<DtsArticleSubjectDto> findSubjectListBySubjectName(String name, Pageable pageable) {
+		JPAQuery<?> jpaQuery = query
+				.from(qArticleSubject)
+				.leftJoin(qAuthor).on(
+						qAuthor.authorId.eq(qArticleSubject.authorId))
+				.where(qArticleSubject.subjectName.like('%' + name + '%'));
 
-        Long fetchOne = jpaQuery
-                .select(qArticleSubject.subjectId.count())
-                .fetchOne();
+		Long fetchOne = jpaQuery
+				.select(qArticleSubject.subjectId.count())
+				.fetchOne();
 
-        List<DtsArticleSubjectDto> fetch = jpaQuery
-                .select(Projections.bean(
-                        DtsArticleSubjectDto.class,
-                        Wildcard.all))
-                .offset(pageable.getOffset())
-                .limit(pageable.getPageSize())
-                .fetch();
+		List<DtsArticleSubjectDto> fetch = jpaQuery
+				.select(Projections.bean(
+						DtsArticleSubjectDto.class,
+						Wildcard.all))
+				.offset(pageable.getOffset())
+				.limit(pageable.getPageSize())
+				.fetch();
 
-        return new PageImpl<>(fetch, pageable, fetchOne);
-    }
+		return new PageImpl<>(fetch, pageable, fetchOne);
+	}
 
-    public String findSubjectIdByArticleId(String articleId) {
-        return query
-                .select(qSubjectRelevance.subjectId)
-                .from(qSubjectRelevance)
-                .where(qSubjectRelevance.articleId.eq(articleId))
-                .fetchOne();
-    }
+	public String findSubjectIdByArticleId(String articleId) {
+		return query
+				.select(qSubjectRelevance.subjectId)
+				.from(qSubjectRelevance)
+				.where(qSubjectRelevance.articleId.eq(articleId))
+				.fetchOne();
+	}
 
-    public List<DtsArticle> findAllBySubjectId(String subjectId) {
-        return query
-                .select(qArticle)
-                .from(qArticle, qSubjectRelevance)
-                .where(
-                        qArticle.articleId.eq(qSubjectRelevance.articleId),
-                        qSubjectRelevance.subjectId.eq(subjectId))
-                .fetch();
-    }
+	public List<DtsArticle> findAllBySubjectId(String subjectId) {
+		return query
+				.select(qArticle)
+				.from(qArticle, qSubjectRelevance)
+				.where(
+						qArticle.articleId.eq(qSubjectRelevance.articleId),
+						qSubjectRelevance.subjectId.eq(subjectId))
+				.fetch();
+	}
 
-    public List<DtsArticle> findSubjectArticleTitleListBySubjectId(String subjectId) {
-        return query
-                .select(Projections.bean(
-                        DtsArticle.class,
-                        qArticle.articleId,
-                        qArticle.title))
-                .from(qArticle, qSubjectRelevance)
-                .where(
-                        qArticle.articleId.eq(qSubjectRelevance.articleId),
-                        qSubjectRelevance.subjectId.eq(subjectId))
-                .fetch();
-    }
+	public List<DtsArticle> findSubjectArticleTitleListBySubjectId(String subjectId) {
+		return query
+				.select(Projections.bean(
+						DtsArticle.class,
+						qArticle.articleId,
+						qArticle.title))
+				.from(qArticle, qSubjectRelevance)
+				.where(
+						qArticle.articleId.eq(qSubjectRelevance.articleId),
+						qSubjectRelevance.subjectId.eq(subjectId))
+				.fetch();
+	}
 
-    public void deleteBySubjectIdAndAuthorId(String subjectId, String authorId) {
-        query.delete(qArticleSubject)
-                .where(
-                        qArticleSubject.authorId.eq(authorId),
-                        qArticleSubject.subjectId.eq(subjectId))
-                .execute();
-    }
+	public void deleteBySubjectIdAndAuthorId(String subjectId, String authorId) {
+		query.delete(qArticleSubject)
+				.where(
+						qArticleSubject.authorId.eq(authorId),
+						qArticleSubject.subjectId.eq(subjectId))
+				.execute();
+	}
 }
--- tang-vue-web/src/views/admin/article.vue
+++ tang-vue-web/src/views/admin/article.vue
@@ -9,22 +9,26 @@
       </el-select>
     </el-form-item>
   </el-form>
-  <el-table :data="noticeList" style="width: 100%">
-    <el-table-column label="标题">
+  <el-table :data="articleList" style="width: 100%" >
+    <el-table-column label="标题" show-overflow-tooltip>
       <template #default="scope">
         {{ scope.row.title }}
       </template>
     </el-table-column>
-    <el-table-column label="路径" prop="url">
+    <el-table-column label="时间">
       <template #default="scope">
-        {{ scope.row.title }}
+        {{ scope.row.createDate }}
+      </template>
+    </el-table-column>
+    <el-table-column label="作者">
+      <template #default="scope">
+        {{ scope.row.nickname }}
       </template>
     </el-table-column>
     <el-table-column align="right">
       <template #header>操作</template>
       <template #default="scope">
         <el-button size="small" type="danger">查看</el-button>
-        <el-button size="small" type="danger" @click="editIndex = scope.$index,insertDialogVisible = true">操作</el-button>
         <el-popconfirm confirm-button-text="确认" cancel-button-text="取消" :icon="InfoFilled" icon-color="red" @confirm="deleteNotice(scope.$index, scope.row)" title="是否确认删除?">
           <template #reference>
             <el-button size="small" type="danger">删除</el-button>
@@ -33,42 +37,6 @@
       </template>
     </el-table-column>
   </el-table>
-
-  <el-dialog v-model="insertDialogVisible" title="操作" width="60%" height="80%">
-    <el-form ref="formRef" :model="form" label-width="120px">
-      <el-form-item label="标题">
-        <el-input v-model="form.title"></el-input>
-      </el-form-item>
-      <el-form-item label="内容">
-        <el-input v-model="form.url"></el-input>
-      </el-form-item>
-    </el-form>
-    <template #footer>
-      <span class="dialog-footer">
-        <el-button @click="insertDialogVisible = false">取消</el-button>
-        <el-button type="primary" @click="insertNotice()">保存</el-button>
-      </span>
-    </template>
-  </el-dialog>
-
-  <el-dialog v-model="articleInfoDialog" title="操作" width="60%" height="80%">
-    <el-form ref="formRef" :model="form" label-width="120px">
-      <el-form-item label="标题">
-        <el-input v-model="form.title"></el-input>
-      </el-form-item>
-      <el-form-item label="内容">
-        <div>
-          
-        </div>
-      </el-form-item>
-    </el-form>
-    <template #footer>
-      <span class="dialog-footer">
-        <el-button @click="insertDialogVisible = false">取消</el-button>
-        <el-button type="primary" @click="insertNotice()">保存</el-button>
-      </span>
-    </template>
-  </el-dialog>
 </template>
   
 <script>
@@ -78,88 +46,38 @@ export default {
   name: "admin_article",
   data() {
     return {
-      insertDialogVisible: false,
-      articleInfoDialog: false,
       routeName: this.$route.name,
-      noticeList: [{}, {}],
+      articleList: [],
       editIndex: -1,
       orderButton: true,
       form: {
-        state: ""
+        state: "1"
       }
     };
   },
   created() { },
   methods: {
-    insertNotice() {
-      request({
-        url: "/api/admin/notice",
-        method: "POST",
-        data: this.form,
-      }).then((response) => {
-        ElMessage({ type: 'success', message: '保存成功' })
-        this.insertDialogVisible = false
-        this.loadRecommend()
-        this.form = {}
-      });
-    },
     deleteNotice(index, row) {
+      alert(row.articleId)
       request({
-        url: "/api/admin/notice/" + row.noticeId,
+        url: "/api/admin/article/" + row.articleId,
         method: "DELETE",
       }).then((response) => {
         this.noticeList.splice(index, 1)
         ElMessage({ type: 'success', message: '删除成功' })
       });
     },
-    updateOrder() {
-      console.log(this.noticeList)
+    loadArticle() {
       request({
-        url: "/api/admin/notice/order",
-        method: "PUT",
-        data: this.noticeList,
-      }).then((response) => {
-        this.orderButton = true
-        ElMessage({ type: 'success', message: '保存成功' })
-      });
-    },
-    saveNotice(index, row) {
-      request({
-        url: "/api/admin/notice",
-        method: "PUT",
-        data: row,
-      }).then((response) => {
-        this.editIndex = -1
-      });
-    },
-    loadRecommend() {
-      request({
-        url: "/api/admin/notice",
+        url: "/api/admin/article",
         method: "GET",
       }).then((response) => {
-        this.noticeList = response.data
+        this.articleList = response.data.content
       });
-    },
-    moveUpward(index, row) {
-      if (index > 0) {
-        let upData = this.noticeList[index - 1];
-        this.noticeList.splice(index - 1, 1);
-        this.noticeList.splice(index, 0, upData);
-        this.orderButton = false
-      }
-    },
-    moveDown(index, row) {
-      if ((index + 1) == this.noticeList.length) {
-      } else {
-        let downData = this.noticeList[index + 1];
-        this.noticeList.splice(index + 1, 1);
-        this.noticeList.splice(index, 0, downData);
-        this.orderButton = false
-      }
     }
   },
   mounted() {
-    this.loadRecommend()
+    this.loadArticle()
   },
 };
 </script>
