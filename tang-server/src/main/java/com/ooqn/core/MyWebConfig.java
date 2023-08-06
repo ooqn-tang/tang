4d6c54849754389f792cafa0c748f4d8877e2d70
bobcbui <bobcbui@outlook.com>
2023-08-06 20:26:08 +0800

    asdfasdf

---
+++ tang-server/src/main/java/com/ooqn/core/MyWebConfig.java
@@ -0,0 +1,18 @@
+package com.ooqn.core;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class MyWebConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
