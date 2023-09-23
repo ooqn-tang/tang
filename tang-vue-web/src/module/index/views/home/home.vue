2535158ca55b455f86302ae1968f132281465fc0
Bobcbui <bobcbui@outlook.com>
2023-09-23 23:20:48 +0800

    修复了发布按钮浏览器拦截问题

--- tang-vue-web/src/module/index/views/home/home.vue
+++ tang-vue-web/src/module/index/views/home/home.vue
@@ -116,7 +116,7 @@ function createArticle(){
     let a = document.createElement("a");           
     a.setAttribute("href", routeData.href);      
     a.setAttribute("target", "_blank");      
-    a.setAttribute("id", id);       
+    a.setAttribute("id", "article-editor");       
     // 防止反复添加      
     if(!document.getElementById(id)) {                               
         document.body.appendChild(a);      
