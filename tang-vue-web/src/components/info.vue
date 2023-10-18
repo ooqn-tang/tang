15b97e54f40ba32e31df5ed08430e09d512ec813
Bobcbui <bobcbui@outlook.com>
2023-10-18 21:01:53 +0800

    前端发布 修改文字

--- tang-vue-web/src/components/info.vue
+++ tang-vue-web/src/components/info.vue
@@ -2,10 +2,10 @@
   <div class="card mb-2">
     <div class="card-body">
       <p>
-        <a href="https://ttcxy.net/post/0b0d396713a54e2fbf714478d740e53e" target="_blank">关于</a>
+        <a href="https://ooqn.com" target="_blank">关于</a>
       </p>
       <p>
-        <a href="https://ttcxy.net/post/98b255d539f743e193e398bfa9b97cfd" target="_blank">友情链接</a>
+        <a href="https://ooqn.com" target="_blank">友情链接</a>
       </p>
       <p>
         <a href="http://beian.miit.gov.cn" target="_blank">湘ICP备20009234号</a>
@@ -14,11 +14,6 @@
     </div>
   </div>
 </template>
-<script setup> 
-import { ref } from 'vue';
-
-let count = ref(0);
-let advList = ref([]);
-let zz = ref(false);
+<script setup>
 
 </script>
