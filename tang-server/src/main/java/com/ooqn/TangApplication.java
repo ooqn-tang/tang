a316c484107430633110a45a1881a90ee2e6fc0d
Bobcbui <bobcbui@outlook.com>
2023-12-21 22:05:34 +0800

    456

--- tang-server/src/main/java/com/ooqn/TangApplication.java
+++ tang-server/src/main/java/com/ooqn/TangApplication.java
@@ -2,7 +2,6 @@ package com.ooqn;
 
 import java.sql.SQLException;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
